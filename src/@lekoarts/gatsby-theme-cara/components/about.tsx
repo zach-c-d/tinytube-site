461741ed52b1db7171a03e768ba335ce3ff994ab
chalmers <info@chalmers.cards>
2025-06-02 11:37:05 -0400

    Adds photos to about section.

--- src/@lekoarts/gatsby-theme-cara/components/about.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/about.tsx
@@ -1,35 +1,84 @@
-import * as React from "react"
-import Divider from "../elements/divider"
-import Inner from "../elements/inner"
-import Content from "../elements/content"
-import Svg from "./svg"
-import { withPrefix } from "gatsby"
-import {StaticImage} from "gatsby-plugin-image"
-import { UpDown, UpDownWide } from "../styles/animations"
-import AboutMDX from "../sections/about.mdx"
-import AboutPipettes from "../sections/about_pipettes.mdx"
+import * as React from "react";
+import Divider from "../elements/divider";
+import Inner from "../elements/inner";
+import Content from "../elements/content";
+import Svg from "./svg";
+import { withPrefix } from "gatsby";
+import AvatarPic from "../../../../static/avatar-pipette.jpg";
+import { StaticImage } from "gatsby-plugin-image";
+import { UpDown, UpDownWide } from "../styles/animations";
+import AboutMDX from "../sections/about.mdx";
+import AboutPipettes from "../sections/about_pipettes.mdx";
 
+import { Card, Text } from "theme-ui";
 
 const About = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
   <div>
-    <Content speed={0.4} offset={0.5} factor={factor}>
+    <Content speed={0.35} offset={offset} factor={factor}>
       <Inner>
         <AboutMDX />
+        <Card
+          sx={{
+            // height: `100%`,
+            maxWidth: `100%`,
+            padding: `1%`,
+            background: "linear-gradient(to right, #D4145A 0%, #FBB03B 100%)",
+            transition: `all 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275) !important`,
+            borderRadius: 8,
+            boxShadow: "0 0 8px rgba(0, 0, 0, 0.125)",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "normal",
+            alignContent: "normal",
+          }}
+        >
+          <Card
+            sx={{
+              display: "block",
+              // flexGrow: 1,
+              // maxWidth: `50%`,
+              height: `100%`,
+              flexGrow: 1,
+              padding: `1%`,
+            }}
+          >
+            <StaticImage
+              src="../images/avatar-pipette.png"
+              sx={{ height: `100%` }}
+            ></StaticImage>
+            <Text>(Avatar 2009)</Text>
+          </Card>
+          <Card
+            sx={{
+              display: "block",
+              flexGrow: 1,
+              // maxWidth: `50%`,
+              height: `100%`,
+              padding: `1%`,
+            }}
+          >
+            <StaticImage
+              src="../images/ncis-pipette.webp"
+              sx={{ height: `100%` }}
+            ></StaticImage>
+            <Text>(CSI:Vegas 2021)</Text>
+          </Card>
+        </Card>
+
+        <AboutPipettes />
       </Inner>
     </Content>
+    {/* <Content speed={0.4} offset={0.75} factor={factor}>
+      <Inner></Inner>
+    </Content> */}
     <Divider
       bg="divider"
       clipPath="polygon(0 16%, 100% 4%, 100% 82%, 0 94%)"
       speed={0.2}
-      offset={offset*1.5}
-      factor={factor*1.5}
+      offset={offset * 1.5}
+      factor={factor * 1.5}
     />
-    {/* <Content speed={0.4} offset={0.8} factor={factor}>
-      <Inner>
-        <AboutPipettes />
-
-      </Inner>
-    </Content> */}
     {/* <StaticImage src="../../static/avatar-pipette.png" width={600} alt="pipette in avatar" /> */}
     {/* <Divider
       bg="divider"
@@ -45,6 +94,6 @@ const About = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
       </UpDownWide>
     </Divider> */}
   </div>
-)
+);
 
-export default About
+export default About;
--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -1,24 +1,24 @@
-import * as React from "react"
-import type { HeadFC } from "gatsby"
-import { Parallax } from "@react-spring/parallax"
-import Layout from "../components/layout"
-import Hero from "../components/hero"
-import Projects from "../components/projects"
-import About from "../components/about"
-import Contact from "../components/contact"
-import Seo from "../components/seo"
+import * as React from "react";
+import type { HeadFC } from "gatsby";
+import { Parallax } from "@react-spring/parallax";
+import Layout from "../components/layout";
+import Hero from "../components/hero";
+import Projects from "../components/projects";
+import About from "../components/about";
+import Contact from "../components/contact";
+import Seo from "../components/seo";
 
 const Cara = () => (
   <Layout>
     <Parallax pages={3}>
       <Hero offset={0} factor={1} />
-      <About offset={1} factor={2} />
-      <Projects offset={1.5} factor={1} />
+      <About offset={0.999} factor={1} />
+      <Projects offset={1.75} factor={1} />
       <Contact offset={2} factor={1} />
     </Parallax>
   </Layout>
-)
+);
 
-export default Cara
+export default Cara;
 
-export const Head: HeadFC = () => <Seo />
+export const Head: HeadFC = () => <Seo />;
